83f50b7a5aed46c047fc60cdb8215c84411c3ddf
YizYah <yyakovson613@gmail.com>
2019-12-09 15:42:46 +0200

    generated no-stack code :tada:

---
+++ src/config/index.js
@@ -0,0 +1,46 @@
+// This file was generated automatically by nostack for the app 'testapp3'.  If you build a new stack from a template, you should replace it.
+// If you modify your current stack to add actions, sources, or types, it would make sense to add constants here.
+
+// platform id
+export const PLATFORM_ID = 'us-west-2_mbkJXtFOb';
+
+// action ids
+  // CREATE_INSTANCE
+export const CREATE_PROJECT_FOR_PROJECTS_FOR_USER_ACTION_ID='e8f87b38-2092-4000-aa34-28473e717398';
+export const CREATE_TASK_FOR_TASK_INFO_ACTION_ID='1d93266f-a7f0-448f-b416-2a04485a9b9a';
+export const CREATE_COMPLETED_FOR_TASK_INFO_ACTION_ID='222eb8a5-2f2f-4e88-b1c7-68cd68c6b511';
+export const CREATE_STEP_FOR_TASK_INFO_ACTION_ID='99f18f95-aa2d-4b08-8439-7c7a39a9da6d';
+export const CREATE_GOAL_FOR_TASK_INFO_ACTION_ID='003fe55a-945c-44de-bf1d-42209c698db1';
+  // UPDATE_INSTANCE
+export const UPDATE_PROJECT_FOR_PROJECTS_FOR_USER_ACTION_ID='21a1d50b-8872-44bd-9618-0f06258f7f45';
+export const UPDATE_TASK_FOR_TASK_INFO_ACTION_ID='6b678e1a-ee31-4d6e-8387-5326bb32c128';
+export const UPDATE_COMPLETED_FOR_TASK_INFO_ACTION_ID='4ff648f6-75c5-4b5b-8d94-d96216b4941a';
+export const UPDATE_STEP_FOR_TASK_INFO_ACTION_ID='8748af6d-de1d-4295-8637-7d49e74e0ef3';
+export const UPDATE_GOAL_FOR_TASK_INFO_ACTION_ID='c18ee5fe-7ad1-44d5-9ade-f9a3e6e3d081';
+  // DELETE_INSTANCE
+export const DELETE_PROJECT_FOR_PROJECTS_FOR_USER_ACTION_ID='af0539f0-6681-455e-928b-fc8ea55a65bd';
+export const DELETE_TASK_FOR_TASK_INFO_ACTION_ID='25c26977-0d55-4e78-bafb-ce529c3a763f';
+export const DELETE_COMPLETED_FOR_TASK_INFO_ACTION_ID='9ec0c9b0-a0ec-496f-83a5-514cdea0d8c9';
+export const DELETE_STEP_FOR_TASK_INFO_ACTION_ID='ea33219e-e6b1-431d-813c-b6b193904dcc';
+export const DELETE_GOAL_FOR_TASK_INFO_ACTION_ID='6ee7b8d1-0dd1-486c-afe0-d4cdba3526e3';
+  // ADD_INSTANCE_ASSN
+export const CREATE_USER_FOR_TASK_INFO_ACTION_ID='d550e730-b3f5-4e10-bd6b-03c7f1196136';
+  // UPDATE_INSTANCE_ASSN
+export const UPDATE_USER_FOR_TASK_INFO_ACTION_ID='3cc7594e-bc65-4d76-8c04-9b5d1513a813';
+  // DELETE_INSTANCE_ASSN
+export const DELETE_USER_FOR_TASK_INFO_ACTION_ID='a623564b-cac8-461a-8011-864740106364';
+  // CREATE_INSTANCE_WITH_UNOWNED_PARENT
+
+// source ids
+export const SOURCE_PROJECTS_FOR_USER_ID='f484c03f-6c7e-4840-aa17-41f3424ab741';
+export const SOURCE_TASK_INFO_ID='b514b63c-ca6f-40a1-ac08-f4f42d0a3560';
+export const SOURCE_ALL_USERS_ID='4c975286-1dc2-4f24-8004-70f55095f508';
+
+// type ids
+export const TYPE_MODERATOR_ID='us-west-2_mbkJXtFObmoderatorClass';
+export const TYPE_USER_ID='c6b63c6c-6063-40f4-8a65-7e346f48c4e4';
+export const TYPE_PROJECT_ID='c1144bc4-cf0e-41b4-a377-c8b15328f79e';
+export const TYPE_TASK_ID='e4bcaff7-b52f-4fbe-8059-796fa5a541b4';
+export const TYPE_COMPLETED_ID='126b4e59-16f4-48ca-a46e-f4dc386569d9';
+export const TYPE_STEP_ID='aa4c20cd-6a44-48bd-99ed-3d0d5229ef36';
+export const TYPE_GOAL_ID='9083dc31-0928-4222-8553-be4baf16a0e1';
